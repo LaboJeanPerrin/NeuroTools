1a7e6189fd70e3466f81a8a5b33f4d148955a20f
Jean Perrin <Jean Perrin@1P>
2016-08-23 16:13:42 +0200

    Created the Camera.PCO Class

---
+++ Matlab/Classes/+Camera/@PCO/PCO.m
@@ -0,0 +1,26 @@
+classdef PCO<handle
+%PCO Class
+    
+    % --- PROPERTIES ------------------------------------------------------
+    properties (Access = public)
+        
+        
+        
+    end
+    
+    % --- METHODS ---------------------------------------------------------
+    methods
+        
+        % _________________________________________________________________
+        function this = PCO(varargin)
+        %PCO::constructor
+        
+        
+        end
+    end
+    
+    % --- STATIC METHODS --------------------------------------------------
+    methods (Static)
+      
+   end
+end
