6dff47c2925e3c865fcbe8eb1a34dd2f91d678e2
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-03-06 11:23:11 +0100

    script global fonctionnel

--- Matlab/Utils/script.m
+++ Matlab/Utils/script.m
@@ -82,3 +82,53 @@ rename(param.date,param.run_number);
 DFF_bg(Layers,F);
 
 
+%% quickscript
+% this script does everything in one shot
+
+start_time = tic;
+
+% go in the project directory
+cd /home/ljp/Science/Projects/RLS_Hugo/
+param.cwd = pwd;
+% get focus
+param.date = '2018-01-11';
+param.run_number = 6;
+param.Layers = 3:12; 
+F = NT.Focus({param.cwd, param.date, param.run_number});
+% create binary file from Tif
+tifToMmap(F, {'z', param.Layers});
+% compute drift on mmap
+param.RefLayers = 8:10;
+param.RefIndex = 10; 
+driftCompute(F,{...
+    'RefLayers', param.RefLayers, ...
+    'RefIndex', param.RefIndex, ...
+    'Layers', param.Layers, ...
+    });
+% applies drift
+driftApply(F);
+% define focus on reference stack and take its ROI if existing
+
+Fref = NT.Focus({param.cwd, param.date, 5});
+% create defMap
+mapToReferenceBrain(F, Fref, param.RefIndex);
+% finds ROI using reference brain mask
+% use the mask predefined on the reference brain to find the mask for the
+% current brain, saves autoROI as a mask.mat file
+autoROI(F, Fref)
+% load library to compute baseline
+[~,~] = loadlibrary('/home/ljp/Science/Projects/RLS_Hugo/Programs/NeuroTools/Tools/caTools.so',...
+                    '/home/ljp/Science/Projects/RLS_Hugo/Programs/NeuroTools/Tools/caTools.h');
+% compute baseline using caTools library
+caToolsRunquantile(F, param.Layers);
+% compute gray stack
+createGrayStack(F)
+% compute background
+background = 400;
+% compute DFF
+dff(F, param.Layers, background);
+
+toc(start_time);
+
+
+
